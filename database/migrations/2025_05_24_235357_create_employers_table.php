98560a9217924669cc563d986245fab965b0ada6
Luis Casas <luisscasas45@gmail.com>
2025-06-08 14:15:03 -0600

    FEAT/ Authorization

--- database/migrations/2025_05_24_235357_create_employers_table.php
+++ database/migrations/2025_05_24_235357_create_employers_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
     {
         Schema::create('employers', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(\App\Models\User::class);
             $table->string('name');
             $table->timestamps();
         });
     }
-
 
     /**
      * Reverse the migrations.
